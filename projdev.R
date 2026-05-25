97062ce42f8f97a02f75581eb25b9a886c0926cf
Alex Montanaro <amontanaro1999@gmail.com>
2026-05-25 13:15:40 +1000

    automatically load devtool at start of R session

--- projdev.R
+++ projdev.R
@@ -2,7 +2,7 @@ library(devtools)
 install.packages("git2r")
 #test package called PackT
 dir.create("C:/Users/sodam/OneDrive - Deakin University/Git_Repositories/PackT")
-
+setwd("C:/Users/sodam/OneDrive - Deakin University/Git_Repositories/PackT")
 create_package("C:/Users/sodam/OneDrive - Deakin University/Git_Repositories/PackT")
 
 use_git()
