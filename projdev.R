b5a1fd3d51083a59a4ec2cffc68505ddb58aad8e
Alex Montanaro <amontanaro1999@gmail.com>
2026-05-25 13:09:57 +1000

    Initial commit

---
+++ projdev.R
@@ -0,0 +1,8 @@
+library(devtools)
+install.packages("git2r")
+#test package called PackT
+dir.create("C:/Users/sodam/OneDrive - Deakin University/Git_Repositories/PackT")
+
+create_package("C:/Users/sodam/OneDrive - Deakin University/Git_Repositories/PackT")
+
+use_git()
